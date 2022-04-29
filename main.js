c448c7a5895ab6ad6262fb36e84ab456c1c70112
NathenGD22 <98773554+NathenGD22@users.noreply.github.com>
2022-04-29 09:44:19 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-var capyCoins = 0;
+var CapyCoins = 0;
 
-function capycoinClick(number){
-    capycoins = capycoins + number;
-    document.getElementById("capyCoins").innerHTML = capycoins;
+function capyClick(number){
+    cookies = CapyCoins + number;
+    document.getElementById("CapyCoins").innerHTML = CapyCoins;
 };
 
-var capys = 0;
+var cursors = 0;
 
 function buyCapy(){
-    var capyCost = Math.floor(10 * Math.pow(1.1,cursors));     //works out the cost of this cursor
-    if(capyCoins >= capyCost){                                   //checks that the player can afford the cursor
-        capys = capys + 1;                                   //increases number of cursors
-    	capyCoins = capyCoins - cursorCost;                          //removes the cookies spent
-        document.getElementById('capys').innerHTML = capys;  //updates the number of cursors for the user
-        document.getElementById('capyCoins').innerHTML = capyCoins;  //updates the number of cookies for the user
+    var capyCost = Math.floor(10 * Math.pow(1.1,Capys));     //works out the cost of this cursor
+    if(CapyCoins >= capyCost){                                   //checks that the player can afford the cursor
+        Capys = Capys + 1;                                   //increases number of cursors
+    	CapyCoins = CapyCoins - capyCost;                          //removes the cookies spent
+        document.getElementById('Capys').innerHTML = Capys;  //updates the number of cursors for the user
+        document.getElementById('CapyCoins').innerHTML = CapyCoins;  //updates the number of cookies for the user
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1,capys));       //works out the cost of the next cursor
+    var nextCost = Math.floor(10 * Math.pow(1.1,Capys));       //works out the cost of the next cursor
     document.getElementById('capyCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
 
 window.setInterval(function(){
 	
-	capyClick(capys);
+	capyClick(cursors);
 	
 }, 1000);
