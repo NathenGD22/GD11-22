4dd2e13f61ebb728a3893ac07fa0df51479631f7
NathenGD22 <98773554+NathenGD22@users.noreply.github.com>
2022-04-29 09:35:00 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var capycoins = 0;
 
 function capycoinClick(number){
     capycoins = capycoins + number;
-    document.getElementById("capycoinscookies").innerHTML = capycoins;
+    document.getElementById("capycoins").innerHTML = capycoins;
 };
 
 var capys = 0;
@@ -12,11 +12,11 @@ function buyCapy(){
     if(capycoins >= capyCost){                                   //checks that the player can afford the cursor
         capys = capys + 1;                                   //increases number of cursors
     	capycoins = capycoins - cursorCost;                          //removes the cookies spent
-        document.getElementById('cursors').innerHTML = capys;  //updates the number of cursors for the user
-        document.getElementById('cookies').innerHTML = capycoins;  //updates the number of cookies for the user
+        document.getElementById('capys').innerHTML = capys;  //updates the number of cursors for the user
+        document.getElementById('capyCoins').innerHTML = capycoins;  //updates the number of cookies for the user
     };
     var nextCost = Math.floor(10 * Math.pow(1.1,capys));       //works out the cost of the next cursor
-    document.getElementById('cursorCost').innerHTML = nextCost;  //updates the cursor cost for the user
+    document.getElementById('capyCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
 
 window.setInterval(function(){
